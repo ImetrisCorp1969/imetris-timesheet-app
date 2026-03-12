283a7948aefbb06139e3b8152f2015d05de7440a
ImetrisCorp1969 <Imetristimesheets@gmail.com>
2026-03-12 00:08:59 -0400

    Delete tools directory

--- tools/hash_passwords.py
+++
@@ -1,8 +0,0 @@
-from streamlit_authenticator import Hasher
-
-# Edit this list of plain-text passwords as needed (temporary)
-PLAINS = ["TempPass123!", "AnotherPass!"]
-
-hashed = Hasher(PLAINS).generate()
-for p, h in zip(PLAINS, hashed):
-    print(f"{p} -> {h}")
